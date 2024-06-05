7db292f776a540d565fa26bbaf0eef0235c25cb7
Ravi Kumar Gupta <babaravibbb@gmail.com>
2024-06-05 15:50:56 +0530

    How to Get Deployed Git Branch

---
+++ src/main/java/org/example/howtogetdeployedgitbranch/HowToGetDeployedGitBranchApplication.java
@@ -0,0 +1,13 @@
+package org.example.howtogetdeployedgitbranch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowToGetDeployedGitBranchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowToGetDeployedGitBranchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/howtogetdeployedgitbranch/HowToGetDeployedGitBranchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.howtogetdeployedgitbranch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowToGetDeployedGitBranchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
